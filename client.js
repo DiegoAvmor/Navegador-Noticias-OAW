65f2c42843fee413e906e96d57a1f10e2cd0ffe8
Diego Avila <42280085+DiegoAvmor@users.noreply.github.com>
2020-02-16 20:35:08 -0600

    Added Sublist for day,month and year

--- client.js
+++ client.js
@@ -1,20 +1,24 @@
-getURL();//Obtiene las noticias una vez que se carga
+const MONTHS = [ { "name": "January" }, { "name": "February" }, { "name": "March" }, { "name": "April" }, { "name": "May" }, { "name": "June" }, { "name": "July" }, { "name": "August" }, { "name": "September" }, { "name": "October" }, { "name": "November" }, { "name": "December" } ];
 
+getRSS("retrieved");//Obtiene las noticias una vez que se carga
+
+//Realiza la accion de buscar las noticias en el url introducido
 $('#btn').click(
     function(){
-        let input = $('#searchInput').val();
+        let input = $('#searchInput');
         if(input){
-            getRSS("search",input);
+            getRSS("search",input.val());
+            input.val('');//Se limpia la busqueda
         }
     }
 ); 
 
-function getURL(){
-    getRSS("retrieved");
-}
-
-
-
+/**
+ * Metodo que tiene como funcion la obtencion de las noticias
+ * a partir de la accion y url introducidos
+ * @param {String} action La accion a realizar
+ * @param {String} url la url con la que se efectuara la accion
+ */
 function getRSS(action,url="none"){
     $.ajax({
         url: "server.php",
@@ -38,10 +42,20 @@ function getRSS(action,url="none"){
       });
 }
 
-function setInformation(feed,domList){
+/**
+ * Metodo cuya funcion es la visualizacion de las noticas obtenidas, creacion
+ * de sublistas y asignacion de noticias a su categoria correspondiente
+ * @param {Array} feed Arreglo de noticias a visualizar
+ * @param {Object} domList El Objeto de documento donde se añadira la noticia
+ * @param {String} action La accion con el cual se añadira las noticas a las listas correspondientes
+ */
+function setInformation(feed,domList,action){
     
-    domList.empty();
+    domList.empty();//Eliminacion de la busqueda anterior
     feed.forEach(element => {
+        let dateSelection = dateToCompare(action,element.Date);
+        console.log(dateSelection);
+
         let feedContent = document.createElement('div');
         feedContent.setAttribute('class','list-group-item');
 
@@ -50,7 +64,7 @@ function setInformation(feed,domList){
         tittle.setAttribute('href',element.TittleURL); //Se adiciona el url de la noticia al encabezado
         feedContent.appendChild(tittle);
 
-        let descriptionContent = document.createElement('div');
+        let descriptionContent = document.createElement('div');//Creacion del cuerpo de la noticia
         let author = document.createElement('p');
         author.setAttribute('class','text-justify');
         author.innerHTML = element.Author;
@@ -62,9 +76,43 @@ function setInformation(feed,domList){
         descriptionContent.appendChild(description);
         feedContent.appendChild(descriptionContent);
 
-        domList.append(feedContent);
+        if(checkIfExists(dateSelection)){//Verifica si una sublista existe y en base a eso hace lo correspondiente
+            let sublist = $('div#'+dateSelection);
+            sublist.append(feedContent);
+        }else{
+            createNewList(dateSelection,domList);
+            let sublist = $('div#'+dateSelection);
+            sublist.append(feedContent);
+        }
 
     });
+}
+
+/**
+ * 
+ * @param {String} listname 
+ * @param {Object} list 
+ */
+function createNewList(listname,list){
+    let collapsableArrow = document.createElement('a');
+    collapsableArrow.setAttribute('href','#'+listname);
+    collapsableArrow.setAttribute('data-toggle',"collapse");
+    collapsableArrow.setAttribute('class','list-group-item');
+    let collapsableTittle = document.createElement('i');
+    collapsableTittle.setAttribute('class','glyphicon glyphicon-chevron-right');
+
+    //Encabezado de la lista colapsable
+    collapsableArrow.append(collapsableTittle);
+    collapsableArrow.append(listname);
+
+    list.append(collapsableArrow);
+
+    //Se crea el apartado para el contenido
+    let collapsableContent = document.createElement('div');
+    collapsableContent.setAttribute('class','list-group collapse');
+    collapsableContent.setAttribute('id',listname);
+
+    list.append(collapsableContent);
 }
 
 
@@ -76,7 +124,7 @@ function sortByDate(array){
         return firstYear < secondYear ? -1 : firstYear > secondYear ? 1 : 0
     });
     domList = $('div#order-year');
-    setInformation(orderedByYear,domList);
+    setInformation(orderedByYear,domList,"Year");
     
     let orderedByMonth = array.slice().sort(function(a,b){
         let firstMonth = new Date(a.Date).getMonth();
@@ -84,7 +132,7 @@ function sortByDate(array){
         return firstMonth < secondMonth ? -1 : firstMonth > secondMonth ? 1 : 0
     });
     domList = $('div#order-month');
-    setInformation(orderedByMonth,domList);
+    setInformation(orderedByMonth,domList,"Month");
 
 
     let orderedByDay = array.slice().sort(function(a,b){
@@ -93,18 +141,48 @@ function sortByDate(array){
         return firstDay < secondDay ? -1 : firstDay > secondDay ? 1 : 0
     });
     domList = $('div#order-day');
-    setInformation(orderedByDay,domList);
+    setInformation(orderedByDay,domList,"Day");
 
 }
 
 
-//Collapse items
+//Realiza el colapso de las sublistas
 $(function() {
-        
     $('.list-group-item').on('click', function() {
       $('.glyphicon', this)
         .toggleClass('glyphicon-chevron-right')
         .toggleClass('glyphicon-chevron-down');
     });
   
-  });
+});
+
+/**
+ * Metodo que verifica si existe la estructura indicado en el DOM
+ * @param {Object} item El objeto a verificar la existencia
+ */
+function checkIfExists(item){
+    if($('div#' + item).length){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+/**
+ * Metodo cuya funcion es obtener el tipo de fecha correspondiente a 
+ * la accion que se pide.
+ * @param {String} action La accion con la que obtendra el tipo de fecha correspondiente
+ * @param {String} dateValue La fecha de la noticia
+ */
+function dateToCompare(action,dateValue){
+    let selectedDate = new Date(dateValue);
+    if(action === "Year"){
+        return selectedDate.getUTCFullYear();
+    }
+    if(action === "Month"){
+        return MONTHS[selectedDate.getUTCMonth()].name;
+    }
+    if(action === "Day"){
+        return selectedDate.getUTCDate();
+    }
+}
