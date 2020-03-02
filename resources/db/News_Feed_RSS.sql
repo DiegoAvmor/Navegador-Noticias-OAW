ba81d8ef46fdedab021e90e96385a7add0527ffb
Hernán Cervera <hjcerveram@hotmail.com>
2020-03-02 11:55:26 -0600

    Update database schema definition file
    This schema is NOT backwards compatible with the previous schema

--- resources/db/News_Feed_RSS.sql
+++ resources/db/News_Feed_RSS.sql
@@ -1,52 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 28-02-2020 a las 02:41:08
--- Versión del servidor: 8.0.18
--- Versión de PHP: 7.3.11
+/* Database schema */
+CREATE DATABASE IF NOT EXISTS news_feed_rss;
+USE news_feed_rss;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `News_Feed_RSS`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `news`
---
-
-CREATE TABLE `news` (
-  `title` varchar(100) NOT NULL,
-  `link` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `author` char(40) DEFAULT NULL,
-  `pub_date` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `news`
---
-ALTER TABLE `news`
-  ADD PRIMARY KEY (`title`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE news (
+  news_id integer PRIMARY KEY AUTO_INCREMENT,
+  title varchar(100) NOT NULL UNIQUE,
+  url varchar(100) NOT NULL,
+  description text NOT NULL,
+  author varchar(40) DEFAULT NULL,
+  publish_date varchar(50) DEFAULT NULL
+);
