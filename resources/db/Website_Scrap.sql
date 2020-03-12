25ce2ac712aac100a887f2ba7d6a0f28f778e658
JohnCaceres <43764825+JohnCaceres@users.noreply.github.com>
2020-03-12 12:58:06 -0600

    Added website reflexive relation table
    -Added a table on the database that represents the reflexive relation between two websites.

--- resources/db/Website_Scrap.sql
+++ resources/db/Website_Scrap.sql
@@ -8,3 +8,12 @@ CREATE TABLE web_scrapping (
   last_modified date DEFAULT NULL,
   FULLTEXT(raw)
 );
+CREATE TABLE references (
+  father_url varchar(100) NOT NULL,
+  child_url varchar(100) NOT NULL,
+   FOREIGN KEY(father_url) 
+   REFERENCES news(url),
+   FOREIGN KEY(child_url) 
+   REFERENCES news(url),
+   CONSTRAINT relationship_not_repeated UNIQUE(father_url, child_url)
+);
